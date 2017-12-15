73f76510e9de44df236a7f81e77c5bf5db101e5c
Marco Bonetti <sid77@users.noreply.github.com>
2017-12-15 17:23:39 +0000

    Add Ubuntu 14.04.1 support

--- cloudatcost_fabric.py
+++ cloudatcost_fabric.py
@@ -42,31 +42,81 @@ def _ssh_config(server_type, ssh_pub_key):
     run('cat /etc/ssh/ssh_host_ed25519_key.pub')
 
 
-def _debian_8(hostname=None, ssh_pub_key=None):
+def _configure_localtime():
     """
-    Deploys a new Debian 8 Cloud At Cost server.
+    Configures local time.
     """
-    # fix crappy, broken cloud at cost setup
+    run('ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime')
+    run('dpkg-reconfigure -f noninteractive tzdata')
+
+
+def _fix_devices_timeout():
+    """
+    Fixes crappy, broken cloud at cost disk setup.
+    """
     run('echo 600 > /sys/block/sda/device/timeout')
     run('echo 600 > /sys/block/sr0/device/timeout')
     put('debian_8/etc/rc.local', '/etc/rc.local')
-    # configure local time
-    run('ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime')
-    run('dpkg-reconfigure -f noninteractive tzdata')
-    # configure hostname
-    sed = 'sed -i -e "s:localhost:{}:g" /etc/hostname'.format(hostname)
+
+
+def _configure_hostname(server_type, hostname):
+    """
+    Configures hostname.
+    """
+    if server_type == 'debian_8':
+        sed = 'sed -i -e "s:localhost:{}:g" /etc/hostname'.format(hostname)
+    elif server_type == 'ubuntu_14':
+        sed = 'sed -i -e "s:ubuntu:{}:g" /etc/hosts /etc/hostname'.format(hostname)
     run(sed)
-    # software upgrade
-    put('debian_8/etc/apt/sources.list', '/etc/apt/sources.list')
-    run('echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list')
+
+
+def _apt_dist_upgrade(options=''):
+    """
+    Runs apt dist-upgrade.
+    """
     run('apt update')
     with shell_env(APT_LISTCHANGES_FRONTEND='none'):
-        run('apt dist-upgrade -y')
-    # install some extra stuff
+        run('apt dist-upgrade -y {}'.format(options))
+
+
+def _debian_8(hostname, ssh_pub_key, server_type):
+    """
+    Deploys a new Debian 8 Cloud At Cost server.
+    """
+    _fix_devices_timeout()
+    _configure_localtime()
+    _configure_hostname(server_type, hostname)
+    put('debian_8/etc/apt/sources.list', '/etc/apt/sources.list')
+    run('echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list')
+    _apt_dist_upgrade()
     run('apt install -y login-duo silversearcher-ag htop firejail tmux unattended-upgrades sudo git irssi')
-    # configure ssh
     _ssh_config('debian_8', ssh_pub_key)
-    # and reboot!
+    run('reboot &')
+    pass
+
+
+def _remove_user(user=None):
+    """
+    Removes a specific user access.
+    """
+    assert(user)
+    run('usermod --expiredate 1 {}'.format(user))
+    run('deluser --remove-all-files {}'.format(user))
+
+
+def _ubuntu_14(hostname, ssh_pub_key, server_type):
+    """
+    Deploys a new Ubuntu 14.04.1 Cloud At Cost server.
+    """
+    _remove_user('user')
+    _fix_devices_timeout()
+    _configure_localtime()
+    _configure_hostname(server_type, hostname)
+    run('echo "deb http://pkg.duosecurity.com/Ubuntu trusty main" > /etc/apt/sources.list.d/duo.list')
+    run('curl -s https://duo.com/APT-GPG-KEY-DUO | apt-key add -')
+    _apt_dist_upgrade('-o Dpkg::Options::="--force-confold"')
+    run('apt install -y login-duo silversearcher-ag htop unattended-upgrades')
+    _ssh_config('ubuntu_14', ssh_pub_key)
     run('reboot &')
     pass
 
@@ -77,7 +127,9 @@ def main():
     """
     args = _parse_args()
     if args.type == 'debian_8':
-        _debian_8(args.hostname, args.ssh_pub_key.name)
+        _debian_8(args.hostname, args.ssh_pub_key.name, 'debian_8')
+    elif args.type == 'ubuntu_14':
+        _ubuntu_14(args.hostname, args.ssh_pub_key.name, 'ubuntu_14')
     else:
         exit('Unkown server distribution type.')
 
